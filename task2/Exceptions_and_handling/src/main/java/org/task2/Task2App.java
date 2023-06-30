9e72c357ab4e389c0db3b5eccde5e663633bc4d6
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-06-30 12:38:31 +0300

    task2 init

---
+++ task2/Exceptions_and_handling/src/main/java/org/task2/Task2App.java
@@ -0,0 +1,13 @@
+package org.task2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Task2App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
